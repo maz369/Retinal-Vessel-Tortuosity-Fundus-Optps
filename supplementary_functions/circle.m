cc72f59db80fe7644b68d156038ec434190a295d
Maz M. Khansari <maziyar.khansari@gmail.com>
2020-07-05 15:16:24 -0700

    Update circle.m

--- supplementary_functions/circle.m
+++ supplementary_functions/circle.m
@@ -3,10 +3,10 @@
 function h = circle(x,y,r)
 
 hold on
-th    = 0:pi/50:2*pi;
+th = 0:pi/50:2*pi;
 xunit = r * cos(th) + x;
 yunit = r * sin(th) + y;
-h     = plot(xunit, yunit,'y');
+h = plot(xunit, yunit,'y');
 h(1).LineWidth = 2;
 hold off
 
